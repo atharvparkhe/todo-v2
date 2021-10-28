5d9716debe2bb2857c9aa4ac7b0ebf8101b07c3e
Atharva Parkhe <patharv777@gmail.com>
2021-10-28 22:57:00 +0530

    added img

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from authentication.models import UserModel
 
 class TodoModel(BaseModel):
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to="img", null=True, blank=True)
     title = models.CharField(max_length=50)
     description = models.TextField()
     is_completed = models.BooleanField(default=False)
